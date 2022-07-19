df9ce8736789080377f8114c923e313ca9d545e4
elias <you@example.com>
2022-07-20 01:12:01 +0300

    first round create student list in db

---
+++ student_management/students/models.py
@@ -0,0 +1,12 @@
+
+from django.db import models
+
+class Students(models.Model):
+    student_number=models.PositiveBigIntegerField()
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+    field_if_study=models.CharField(max_length=50)
+    gpa=models.FloatField()
+    def __str__(self):
+        return f'Students: {self.first_name} {self.last_name}'
